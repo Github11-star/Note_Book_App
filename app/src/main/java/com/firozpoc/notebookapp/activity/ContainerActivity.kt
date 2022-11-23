84abd938a0a4ce5ddb838b56f3b24d56022dc1f0
Firoz Shaikh <firoz.md@atos.net>
2022-11-23 11:18:14 +0530

    Initial commit

---
+++ app/src/main/java/com/firozpoc/notebookapp/activity/ContainerActivity.kt
@@ -0,0 +1,12 @@
+package com.firozpoc.notebookapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firozpoc.notebookapp.R
+
+class ContainerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+    }
+}
---
+++ app/src/main/java/com/firozpoc/notebookapp/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.firozpoc.notebookapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.firozpoc.notebookapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this,ContainerActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
